c7831d020ada05d1f83d8c1824a007ebb7a63bae
Pavan Poudel <poudelpavan@users.noreply.github.com>
2017-08-02 20:10:40 -0400

    updated code

--- arduino_code.ino
+++ arduino_code.ino
@@ -1,27 +1,26 @@
 #include <DHT.h>
-
 #include "DHT.h"
 
 #define DHTPIN 2     // what pin we're connected to
-
 #define DHTTYPE DHT11   // we are using the DHT11 sensor
 
 DHT dht(DHTPIN, DHTTYPE);
 
 #define DC_MOTOR_PIN 1
 
-const int led=6;
+const int LED_PIN=6;
+const int g_pin = 5, y_pin = 4, r_pin = 3;
 
 void setup() {
   /* Initialize DC motor control pin as digital output */
   pinMode( DC_MOTOR_PIN, OUTPUT );
-  pinMode(led, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(g_pin, OUTPUT);
+  pinMode(y_pin, OUTPUT);
+  pinMode(r_pin, OUTPUT);
 
-  for (int DigitalPin = 3; DigitalPin <= 5; DigitalPin++) 
-  {
-    pinMode(DigitalPin, OUTPUT);
-  }
   dht.begin();
+  
   //Serial.begin(9600);
   //Serial.setTimeout(1000);
 }
@@ -29,17 +28,16 @@ void setup() {
 void loop() {
 /*Control light*/
   int sensor_value = analogRead(A0);
-  Serial.println(sensor_value);
+  //Serial.println(sensor_value);
   if (sensor_value < 150)// the point at which the state of LEDs change 
     { 
-      digitalWrite(led, HIGH);  //sets LEDs ON
+      digitalWrite(LED_PIN, HIGH);  //sets LEDs ON
     }
   else
     {
-      digitalWrite(led,LOW);  //Sets LEDs OFF
+      digitalWrite(LED_PIN,LOW);  //Sets LEDs OFF
     }
 
-  delay(1000);
   // Reading temperature or humidity takes about 250 milliseconds!
   float h = dht.readHumidity();
   
@@ -50,28 +48,30 @@ void loop() {
 //the 3-led setup process
   if (t<=20)
   {
-  digitalWrite(5, HIGH);
-  digitalWrite(4, LOW);
-  digitalWrite(3, LOW);
+  digitalWrite(g_pin, HIGH);
+  digitalWrite(y_pin, LOW);
+  digitalWrite(r_pin, LOW);
 
   digitalWrite( DC_MOTOR_PIN, 0 );
   Serial.println(analogRead(DC_MOTOR_PIN));
   }
   else if (t>20)
   {
-  digitalWrite(4, HIGH);
-  digitalWrite(5, LOW);
-  digitalWrite(3, LOW);
+  digitalWrite(y_pin, HIGH);
+  digitalWrite(g_pin, LOW);
+  digitalWrite(r_pin, LOW);
 
   digitalWrite( DC_MOTOR_PIN, 1 );
   }
   else if (t>=30)
   {
-  digitalWrite(3, HIGH);
-  digitalWrite(4, LOW);
-  digitalWrite(5, LOW);
+  digitalWrite(r_pin, HIGH);
+  digitalWrite(y_pin, LOW);
+  digitalWrite(g_pin, LOW);
   
   digitalWrite( DC_MOTOR_PIN, 255 );
-  }   
+  }
+  
+  delay(1000);
 }
   
